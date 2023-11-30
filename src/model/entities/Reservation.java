58b8db46d2805d4842d9218cfdec18ed2f66ed44
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-11-29 22:24:47 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,74 @@
+package model.entities;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
+
+public class Reservation {
+	// Atributos
+	private Integer roomNumber;
+	private Date checkIn, checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	// Métodos Getters e Setters
+	
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	/*
+	public void setCheckOut(Date checkOut) {
+		this.checkOut = checkOut;
+	}
+	*/
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	/*
+	public void setCheckIn(Date checkIn) {
+		this.checkIn = checkIn;
+	}
+	*/
+	public Date getCheckIn() {
+		return checkIn;
+	}
+	
+	// Métodos Construtores
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	// Métodos
+	// Criando um método para calcular a duração dos dias
+	public long duration() {
+		// Calculando a diferença entre duas datas (milisegundos)
+		long diff = checkOut.getTime() - checkIn.getTime(); // pega a diferença entre as duas datas em milisegundos
+		// converter de milisegundos para dias (TimeUnit) tipo enumerado complexo
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Converte o valor diff de milesegundos para dias	
+	}
+	
+	// Recebe duas datas novas e atualiza o checkin e checkout 
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	// Imprimir no formato desejado
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+}
